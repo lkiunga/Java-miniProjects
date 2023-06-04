768be84ba6e0eb6143a3f8c3b8d9b0cbdff11a38
lkiunga <linet.kiunga@strathmore.edu>
2023-06-04 21:04:01 +0300

    Java comments

--- DataSizeRange.java
+++ DataSizeRange.java
@@ -4,6 +4,7 @@ class DataSizeRange
 {
 	public static void main(String arg[])
 	{
+		//PrACTISE OF 
 		System.out.println(" Int Min "+ Integer.MIN_VALUE);
 		System.out.println("Int Max" + Integer.MAX_VALUE);
 		//System.out.prinln("Int Byte Size" + Integer.BITCOUNT);	
